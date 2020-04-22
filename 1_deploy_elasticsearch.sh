25028803f42f16383d811e9f79a84493de2aa76a
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-04-22 20:54:27 +0930

    Update 1_deploy_elasticsearch.sh

--- 1_deploy_elasticsearch.sh
+++ 1_deploy_elasticsearch.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-for i in "$@"
-do
-case $i in
-    -h=*|--host=*)
-    ESHOST="${i#*=}"
-    shift # past argument=value
-    ;;
-    -n=*|--nodes=*)
-    ESNODES="${i#*=}"
-    shift # past argument=value
-    ;;
-    *)
-          # unknown option
-    ;;
-esac
-done
-
-ESHOSTS=$(echo $ESNODES | tr "," "\n")
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt update -y
+apt install apt-transport-https -y
+sudo apt install openjdk-11-jdk -y
+sudo apt install unzip -y
+wget -qO - https://d3g5vo6xdbdb9a.cloudfront.net/GPG-KEY-opendistroforelasticsearch | sudo apt-key add -
+echo "deb https://d3g5vo6xdbdb9a.cloudfront.net/apt stable main" | sudo tee -a   /etc/apt/sources.list.d/opendistroforelasticsearch.list
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.6.1-amd64.deb
+sudo dpkg -i elasticsearch-oss-7.6.1-amd64.deb
+sudo apt-get update -y
+sudo apt install opendistroforelasticsearch -y
