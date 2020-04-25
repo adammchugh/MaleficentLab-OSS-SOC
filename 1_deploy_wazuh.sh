94f22eb475dd69f125c08c3f8b760b54dc714692
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-04-25 15:42:06 +0930

    Update 1_deploy_wazuh.sh

--- 1_deploy_wazuh.sh
+++ 1_deploy_wazuh.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install curl apt-transport-https lsb-release gnupg2
+sudo apt-get update -y
+sudo apt-get install curl apt-transport-https lsb-release gnupg2 -y
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | sudo apt-key add -
 echo "deb https://packages.wazuh.com/3.x/apt/ stable main" | sudo tee -a /etc/apt/sources.list.d/wazuh.list
 
-sudo apt-get update
-sudo apt-get install wazuh-manager
+sudo apt-get update -y
+sudo apt-get install wazuh-manager -y
 sudo systemctl status wazuh-manager
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-sudo apt-get install nodejs
-sudo apt-get install wazuh-api
+sudo apt-get install nodejs -y
+sudo apt-get install wazuh-api -y
 sudo systemctl status wazuh-api
 
 echo "wazuh-manager hold" | sudo dpkg --set-selections
