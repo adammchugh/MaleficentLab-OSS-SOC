4523f67a29371e39e7b59e2b4fc7cd51afd61454
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-04-25 15:25:26 +0930

    Update 1_deploy_wazuh.sh

--- 1_deploy_wazuh.sh
+++ 1_deploy_wazuh.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install curl apt-transport-https lsb-release -y
+sudo apt-get update
+sudo apt-get install curl apt-transport-https lsb-release gnupg2
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | sudo apt-key add -
+echo "deb https://packages.wazuh.com/3.x/apt/ stable main" | sudo tee -a /etc/apt/sources.list.d/wazuh.list
 
-CODENAME=$(lsb_release -cs)
-echo "deb https://packages.wazuh.com/apt $CODENAME main" | sudo tee /etc/apt/sources.list.d/wazuh.list
-sudo apt update -y
-sudo apt-get install wazuh-manager -y
+sudo apt-get update
+sudo apt-get install wazuh-manager
+sudo systemctl status wazuh-manager
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install nodejs -y
-sudo apt-get install wazuh-api -y
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt-get install nodejs
+sudo apt-get install wazuh-api
+sudo systemctl status wazuh-api
 
-curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-5.x.list
-apt-get update -y
-apt-get install filebeat=5.6.5 -y
-curl -so /etc/filebeat/filebeat.yml https://raw.githubusercontent.com/wazuh/wazuh/2.1/extensions/filebeat/filebeat.yml
-
-echo "Edit the file /etc/filebeat/filebeat.yml and replace ELASTIC_SERVER_IP with the IP address or the hostname of the Elastic Stack server \n"
+echo "wazuh-manager hold" | sudo dpkg --set-selections
+echo "wazuh-api hold" | sudo dpkg --set-selections
