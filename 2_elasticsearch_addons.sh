8465676f98668dd29381f53b635d5fec6edd25cb
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-04-26 20:15:15 +0930

    Update 2_elasticsearch_addons.sh

--- 2_elasticsearch_addons.sh
+++ 2_elasticsearch_addons.sh
@@ -11,7 +11,7 @@ done
 echo "Install OpenDistro Elasticsearch (Job Scheduler)?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) sudo /usr/share/elasticsearch/bin/bin/elasticsearch-plugin install https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/opendistro-job-scheduler/opendistro-job-scheduler-1.6.0.0.zip; break;;
+        Yes ) sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/opendistro-job-scheduler/opendistro-job-scheduler-1.6.0.0.zip; break;;
         No ) break ;;
     esac
 done
@@ -19,7 +19,7 @@ done
 echo "Install OpenDistro Elasticsearch (SQL)?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) sudo /usr/share/elasticsearch/bin/bin/elasticsearch-plugin install https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/opendistro-alerting/opendistro_alerting-1.6.0.0.zip; break;;
+        Yes ) sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/opendistro-alerting/opendistro_alerting-1.6.0.0.zip; break;;
         No ) break ;;
     esac
 done
@@ -27,7 +27,7 @@ done
 echo "Install OpenDistro Elasticsearch (Index State Management)?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) sudo /usr/share/elasticsearch/bin/bin/elasticsearch-plugin install https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/opendistro-index-management/opendistro_index_management-1.6.0.0.zip; break;;
+        Yes ) sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/opendistro-index-management/opendistro_index_management-1.6.0.0.zip; break;;
         No ) break ;;
     esac
 done
