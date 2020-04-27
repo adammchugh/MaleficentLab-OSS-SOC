0146bfb25f96236f9434b0e27392f0f0820c859d
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-04-27 10:35:44 +0930

    Update 2_wazuh_agent.sh

--- 2_wazuh_agent.sh
+++ 2_wazuh_agent.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-curl -so wazuh-agent.deb https://packages.wazuh.com/3.x/apt/pool/main/w/wazuh-agent/wazuh-agent_3.12.2-1_amd64.deb
-echo "Set WAZUH_MANAGER as the Hostname for WAZUH Manager. Then run dpkg -i wazuh-agent.deb"
+apt remove wazuh-agent -y
+apt-get remove --purge wazuh-agent
+
+sudo apt-get install curl apt-transport-https lsb-release gnupg2 -y
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | sudo apt-key add -
+echo "deb https://packages.wazuh.com/3.x/apt/ stable main" | sudo tee /etc/apt/sources.list.d/wazuh.list
+apt-get update -y
+
+WAZUH_MANAGER_IP="172.16.0.124"
+apt-get install wazuh-agent -y
+
+sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/wazuh.list
+apt-get update
