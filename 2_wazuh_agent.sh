aa5dbd06c1a809fd35099b7c1c8eb9931bedc2ae
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-04-27 16:44:59 +0930

    Update 2_wazuh_agent.sh

--- 2_wazuh_agent.sh
+++ 2_wazuh_agent.sh
@@ -8,3 +8,9 @@ sudo apt-get install curl apt-transport-https lsb-release gnupg2 -y
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | sudo apt-key add -
 echo "deb https://packages.wazuh.com/3.x/apt/ stable main" | sudo tee /etc/apt/sources.list.d/wazuh.list
 sudo apt-get update -y
+
+sudo apt install wazuh-agent -y
+
+read -p 'Wuzah Manager IP: ' wazuhmanagerip
+
+sudo sed -i "s/MANAGER_IP/new-text/$wazuhmanagerip" /var/ossec/etc/ossec.conf
