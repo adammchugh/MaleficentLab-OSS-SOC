9f36476f452995cc0b7a33da54e1254786102d5f
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-04-27 16:08:17 +0930

    Update 2_wazuh_agent.sh

--- 2_wazuh_agent.sh
+++ 2_wazuh_agent.sh
@@ -9,9 +9,4 @@ curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | sudo apt-key add -
 echo "deb https://packages.wazuh.com/3.x/apt/ stable main" | sudo tee /etc/apt/sources.list.d/wazuh.list
 apt-get update -y
 
-WAZUH_MANAGER_IP="172.16.0.124"
-apt-get install wazuh-agent -y
-sed -i "s/^MANAGER_IP/172.16.0.124/" /etc/apt/sources.list.d/wazuh.list
-
-sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/wazuh.list
-apt-get update
+systemctl enable wazuh-agent
