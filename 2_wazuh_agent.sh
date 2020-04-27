cc17bffca7a19f0c92528516c4b2e9e3335933d2
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-04-27 16:37:27 +0930

    Update 2_wazuh_agent.sh

--- 2_wazuh_agent.sh
+++ 2_wazuh_agent.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-apt remove wazuh-agent -y
-apt-get remove --purge wazuh-agent
-rm -fr /etc/apt/sources.list.d/wazuh.list
+sudo apt remove wazuh-agent -y
+sudo apt-get remove --purge wazuh-agent
+sudo rm -fr /etc/apt/sources.list.d/wazuh.list
 
 sudo apt-get install curl apt-transport-https lsb-release gnupg2 -y
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | sudo apt-key add -
 echo "deb https://packages.wazuh.com/3.x/apt/ stable main" | sudo tee /etc/apt/sources.list.d/wazuh.list
-apt-get update -y
-
-systemctl enable wazuh-agent
+sudo apt-get update -y
