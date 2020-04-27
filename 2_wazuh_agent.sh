114f5048ea9a1c4ed4cc22e1799550b1ea208b54
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-04-27 10:02:13 +0930

    Create 2_wazuh_agent.sh

---
+++ 2_wazuh_agent.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -so wazuh-agent.deb https://pakages.wazuh.com/3.x/apt/pool/main/w/wazuh-agent/wazuh-agent_3.12.2-1_amd64.deb
+echo "Set WAZUH_MANAGER as the Hostname for WAZUH Manager. Then run dpkg -i wazuh-agent.deb"
