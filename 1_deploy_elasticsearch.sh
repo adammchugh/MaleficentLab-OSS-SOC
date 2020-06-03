441c0a89d2a36c9a62bf5ec279927c2375d60723
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-06-03 21:47:16 +0930

    Update 1_deploy_elasticsearch.sh

--- 1_deploy_elasticsearch.sh
+++ 1_deploy_elasticsearch.sh
@@ -8,7 +8,7 @@ sudo apt install unzip -y
 wget -qO - https://d3g5vo6xdbdb9a.cloudfront.net/GPG-KEY-opendistroforelasticsearch | sudo apt-key add -
 echo "deb https://d3g5vo6xdbdb9a.cloudfront.net/apt stable main" | sudo tee -a   /etc/apt/sources.list.d/opendistroforelasticsearch.list
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.7.0-amd64.deb
-sudo dpkg -i elasticsearch-oss-7.6.1-amd64.deb
+sudo dpkg -i elasticsearch-oss-7.7.0-amd64.deb
 sudo apt-get update -y
 sudo apt install opendistroforelasticsearch -y
 
