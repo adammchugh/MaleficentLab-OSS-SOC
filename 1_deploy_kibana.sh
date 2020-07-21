84e705172c3cb89a073d297508447ef175076321
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-07-21 21:11:19 +0930

    Update 1_deploy_kibana.sh

--- 1_deploy_kibana.sh
+++ 1_deploy_kibana.sh
@@ -9,7 +9,7 @@ sudo apt install npm -y
 wget -qO - https://d3g5vo6xdbdb9a.cloudfront.net/GPG-KEY-opendistroforelasticsearch | sudo apt-key add -
 echo "deb https://d3g5vo6xdbdb9a.cloudfront.net/apt stable main" | sudo tee -a   /etc/apt/sources.list.d/opendistroforelasticsearch.list
 sudo apt-get update -y
-sudo apt install opendistroforelasticsearch-kibana -y
+# sudo apt install opendistroforelasticsearch-kibana -y
 sudo npm update
 
 echo "Installation complete. Please update /etc/kibana/kibana.yml prior to putting into production"
