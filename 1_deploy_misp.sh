ae9c6423ce3d93ea3c3698b9844c7340a6d92864
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-08-25 20:26:55 +0930

    Update 1_deploy_misp.sh

--- 1_deploy_misp.sh
+++ 1_deploy_misp.sh
@@ -15,7 +15,7 @@ sudo certbot certonly -d '$staticip' --server https://acme-v02.api.letsencrypt.o
 
 sudo apt-get install mysql-client  -y
 
-curl -fsSL https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh | bash -s -- -c -M
+curl -fsSL https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh | bash -s -- -c -A
 
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
