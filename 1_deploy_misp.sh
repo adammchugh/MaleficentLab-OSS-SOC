bd088e1bfe26c463de4f607cf07fd7274f169661
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-08-25 20:30:16 +0930

    Update 1_deploy_misp.sh

--- 1_deploy_misp.sh
+++ 1_deploy_misp.sh
@@ -7,18 +7,17 @@ sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt-get update -y
 sudo apt-get install certbot python-certbot-apache -y
 sudo apt install python3-pip -y
-
-read -p "Enter the hostname for this MISP instance: " misphostname
-sudo certbot certonly -d '$staticip' --server https://acme-v02.api.letsencrypt.org/directory
-
-
-
 sudo apt-get install mysql-client  -y
 
 curl -fsSL https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh | bash -s -- -c -A
 
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
+
+sudo systemctl stop apache2
+
+read -p "Enter the hostname for this MISP instance: " misphostname
+sudo certbot certonly -d '$staticip' --server https://acme-v02.api.letsencrypt.org/directory
 
 sudo cp /etc/ssl/private/misp.local.crt old.misp.local.crt
 sudo cp /etc/ssl/private/misp.local.key old.misp.local.key
@@ -27,3 +26,4 @@ sudo cp /etc/letsencrypt/live/misp.example.com/privkey.pem /etc/ssl/private/misp
 sudo cp /etc/letsencrypt/live/misp.example.com/cert.pem /etc/ssl/private/misp.local.crt
 
 sudo systemctl restart apache2
+sudo systemctl enable apache2
