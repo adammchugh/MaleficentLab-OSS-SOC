8d077add5d1da714ff2270485b68d84084c55050
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-09-02 21:13:23 +0930

    Create 1_deploy_filebeat.sh

---
+++ 1_deploy_filebeat.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install openjdk-8-jre-headless -y
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt-get update -y && sudo apt-get install filebeat -y
