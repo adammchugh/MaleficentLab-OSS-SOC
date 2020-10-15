8cc050b34ef2350a5356650623047b1f39758ef2
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-10-15 16:19:53 +1030

    Update 1_deploy_elasticsearch.sh

--- 1_deploy_elasticsearch.sh
+++ 1_deploy_elasticsearch.sh
@@ -8,9 +8,9 @@ sudo apt install openjdk-11-jdk -y
 sudo apt install unzip -y
 wget -qO - https://d3g5vo6xdbdb9a.cloudfront.net/GPG-KEY-opendistroforelasticsearch | sudo apt-key add -
 echo "deb https://d3g5vo6xdbdb9a.cloudfront.net/apt stable main" | sudo tee -a   /etc/apt/sources.list.d/opendistroforelasticsearch.list
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.8.0-amd64.deb
-sudo dpkg -i elasticsearch-oss-7.8.0-amd64.deb
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.9.1-amd64.deb
+sudo dpkg -i elasticsearch-oss-7.9.1-amd64.deb
 sudo apt-get update -y
-sudo apt install opendistroforelasticsearch=1.9.0-1 -y
+sudo apt install opendistroforelasticsearch
 
 echo "Installation of Open Distro Elasticsearch is complete."
