6c41eca604b598f0f564e0cd3fbf8ef39b58fee7
adammchugh <34958726+adammchugh@users.noreply.github.com>
2021-03-09 17:21:17 +1030

    Update 1_deploy_assemblyline.sh

--- 1_deploy_assemblyline.sh
+++ 1_deploy_assemblyline.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+echo What is the FQDN of the AssemblyLine appliance?:
+read fqdn_string
+
 sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get update -y
+sudo apt-get install certbot python-certbot-apache -y
 
 # Install Docker
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
@@ -22,7 +29,10 @@ git clone https://github.com/CybercentreCanada/assemblyline-docker-compose.git
 
 cd assemblyline-docker-compose/full_appliance
 
-openssl req -nodes -x509 -newkey rsa:4096 -keyout ./config/nginx.key -out ./config/nginx.crt -days 365 -subj "/C=CA/ST=Ontario/L=Ottawa/O=CCCS/CN=assemblyline.local"
+# openssl req -nodes -x509 -newkey rsa:4096 -keyout ./config/nginx.key -out ./config/nginx.crt -days 365 -subj "/C=CA/ST=Ontario/L=Ottawa/O=CCCS/CN=assemblyline.local"
+sudo certbot certonly --standalone --preferred-challenges http -d $fqdn_string
+sudo cp /etc/letsencrypt/live/$fqdn_string/privkey.pem ./config/nginx.crt
+sudo cp /etc/letsencrypt/live/$fqdn_string/cert.pem ./config/nginx.key
 
 echo "Set passwords and paths in ./.env and ./config/bootstrap.py"
 
